4866d16b8e6d97cb7dff6626bde34f81b3f57733
Kacper Klimek <kacperklimek817@gmail.com>
2025-03-26 20:19:31 +0100

    Zadanie 1

--- LABY2/Program.cs
+++ LABY2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Zad1.Zadanie1();
         }
     }
 }
---
+++ LABY2/Zadanie1.cs
@@ -0,0 +1,41 @@
+﻿public class Zad1
+{
+	public Zad1(){ 
+	}
+
+
+		public static void Zadanie1()
+	{
+        Console.WriteLine("Podaj ciąg liczb oddzielonych spacją do posortowania");
+		string bubble_input = Console.ReadLine();
+
+        int[] A = bubble_input.Split(' ').Select(s => int.Parse(s)).ToArray();
+
+		for (int i = 0; i < A.Length; i++)
+		{
+			for (int j = 0; j < A.Length-1; j++)
+			{
+				if (A[j] > A[j+1]) {
+					int a;
+					a = A[j];
+					A[j] = A[j + 1];
+					A[j + 1] = a;
+				}
+
+			}
+
+        }
+        Console.WriteLine("posortowany ciąg");
+		foreach (int i in A) {
+            Console.Write(i + " ");		
+		}
+
+
+
+
+
+    }
+
+
+	
+}
